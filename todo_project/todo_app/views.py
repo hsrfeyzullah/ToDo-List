5130306d5008768dbcad1a87d885c0d2313c9fa0
Feyzullah Hasar <hasarfeyzullah@gmail.com>
2020-12-23 02:53:52 +0300

    sesson verificaion added

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -10,67 +10,76 @@ from django.contrib.auth.forms import UserCreationForm #1
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login
 from functools import wraps
+from django.contrib.sessions.models import Session
 # Create your views here.
 
 #-------------------------
 def index(request):
-    if request.method == "POST":
-        form = ListForm(request.POST or None)
-        if form.is_valid:
-            #post = form.save(commit=False)
-            #post.user = request.user
-           # post.save()
-            form.save()
-            todo_list = ToDos.objects.all()
-            #todo_list = ToDos.objects.all(user = "id")
+    user=request.user.id
+    if session_verification():
+        if request.method == "POST":
+            form = ListForm(request.POST or None)
+            if form.is_valid:
+                form.save()
+                todo_list = ToDos.objects.filter(user=user)
+                return render(request, "todo_app/index.html",{'todo_list':todo_list})
+        else:    
+            #todo_list = ToDos.objects.all()       
+            todo_list = ToDos.objects.filter(user=user)
             return render(request, "todo_app/index.html",{'todo_list':todo_list})
-    else:    
-        todo_list = ToDos.objects.all()
-        return render(request, "todo_app/index.html",{'todo_list':todo_list})
 #--------------------------------------------------------------------------------------------
 def about(request):
     return render(request, "todo_app/about.html")      
 
 #--------------------------------------------------------------------------------------------
 def create(request):
-    if request.method == "POST":
-        form = ListForm(request.POST or None) 
-        if form.is_valid:
-            post = form.save(commit=False)            
+    print(request.user.id)
+    if session_verification():
+    #if request.user.is_authenticated:   
+        if request.method == "POST":
+            form = ListForm(request.POST or None) 
+            if form.is_valid:
+                post = form.save(commit=False)            
+                todo_list = ToDos.objects.all()
+                return render(request, "todo_app/create.html",{'todo_list':todo_list})
+        else:    
             todo_list = ToDos.objects.all()
-            return render(request, "todo_app/create.html",{'todo_list':todo_list})
-    else:    
-        todo_list = ToDos.objects.all()
-        return render(request, "todo_app/create.html",{'todo_list':todo_list})  
-
+            return render(request, "todo_app/create.html",{'todo_list':todo_list})  
+    #else:
+    #    print("*********************")
+    #    return render(request, "accounts/form.html", {'form':form , 'title' : 'Login' })        
 #--------------------------------------------------------------------------------------------
 def delete(request,ToDos_id):
-    todo = ToDos.objects.get(pk=ToDos_id)
-    todo.delete()
-    return redirect("index")
+    if session_verification():
+        todo = ToDos.objects.get(pk=ToDos_id)
+        todo.delete()
+        return redirect("index")
 #---------------------------------------------------------------------------------------------
 def update(request, ToDos_id):
-    if request.method == "POST":
-        todo_update = ToDos.objects.get(pk=ToDos_id)
-        form = ListForm(request.POST or None, instance= todo_update)
-        if form.is_valid:
-            form.save()
-            return redirect("index")
-    else:    
-        todo_list = ToDos.objects.all()
-        return render(request, "todo_app/update.html",{'todo_list':todo_list})  
+    if session_verification():
+        if request.method == "POST":
+            todo_update = ToDos.objects.get(pk=ToDos_id)
+            form = ListForm(request.POST or None, instance= todo_update)
+            if form.is_valid:
+                form.save()
+                return redirect("index")
+        else:    
+            todo_list = ToDos.objects.all()
+            return render(request, "todo_app/update.html",{'todo_list':todo_list})  
 #--------------------------------------------------------------------------------------------
 def yes_finish(request,ToDos_id):
-    todo = ToDos.objects.get(pk=ToDos_id)
-    todo.finished = False   
-    todo.save()
-    return redirect("index")
+    if session_verification():
+        todo = ToDos.objects.get(pk=ToDos_id)
+        todo.finished = False   
+        todo.save()
+        return redirect("index")
 #--------------------------------------------------------------------------------------------
 def no_finish(request,ToDos_id):
-    todo = ToDos.objects.get(pk=ToDos_id)
-    todo.finished = True
-    todo.save()
-    return redirect("index") 
+    if session_verification():
+        todo = ToDos.objects.get(pk=ToDos_id)
+        todo.finished = True
+        todo.save()
+        return redirect("index") 
 #---------------------------------------------------------------------------
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -78,7 +87,8 @@ def login_view(request):
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")      
         user = authenticate(username=username, password=password)
-        login(request, user)  
+        login(request, user)
+        request.session[0]=1  
         return redirect("index")       
     return render(request, "accounts/form.html", {"form":form , 'title' : 'Login' })
 #---------------------------------------------------------------------------
@@ -99,3 +109,12 @@ def register_view(request):
 def logout_view(request):
     logout(request)
     return redirect("logout")
+#---------------------------------------------------------------------------
+def session_verification(request):
+    if request.user.is_authenticated:
+        return True
+    else:
+        print("*********************")
+        return render(request, "accounts/form.html", {"form":form , 'title' : 'Login' })
+
+#---------------------------------------------------------------------------
